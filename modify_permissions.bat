e96a00509e83402211cbf0b77ffab9bedfb5dd80
An-Thanh <nguyenhien.phanhoai@gmail.com>
2025-01-04 14:17:02 +0700

    rebase

--- modify_permissions.bat
+++ modify_permissions.bat
@@ -1,10 +1,10 @@
 @echo off
-icacls "\\192.168.10.25\SDriver\Thanhan\folder" /grant "PBL4\Administrator:(OI)(CI)F"
-icacls "\\192.168.10.25\SDriver\Thanhan\folder" /grant "PBL4\Thanhan:(OI)(CI)F"
-icacls "\\192.168.10.25\SDriver\Thanhan\folder\*" /grant "PBL4\Administrator:(OI)(CI)F" /T
-icacls "\\192.168.10.25\SDriver\Thanhan\folder\*" /grant "PBL4\Thanhan:(OI)(CI)F" /T
-icacls "\\192.168.10.25\SDriver\Thanhan\folder" /inheritance:r
-icacls "\\192.168.10.25\SDriver\Thanhan\folder" /remove "Everyone"
-icacls "\\192.168.10.25\SDriver\Thanhan\folder" /remove "Everyone" /T
-icacls "\\192.168.10.25\SDriver\Thanhan\folder" /grant "Everyone":(OI)(CI)R
-icacls "\\192.168.10.25\SDriver\Thanhan\folder\*" /grant "Everyone":(OI)(CI)R /T
+icacls "\\192.168.10.25\SDriver\Thanhan\Toilampbl4" /grant "PBL4\Administrator:(OI)(CI)F"
+icacls "\\192.168.10.25\SDriver\Thanhan\Toilampbl4" /grant "PBL4\Thanhan:(OI)(CI)F"
+icacls "\\192.168.10.25\SDriver\Thanhan\Toilampbl4\*" /grant "PBL4\Administrator:(OI)(CI)F" /T
+icacls "\\192.168.10.25\SDriver\Thanhan\Toilampbl4\*" /grant "PBL4\Thanhan:(OI)(CI)F" /T
+icacls "\\192.168.10.25\SDriver\Thanhan\Toilampbl4" /inheritance:r
+icacls "\\192.168.10.25\SDriver\Thanhan\Toilampbl4" /remove "PBL4\XPhuc"
+icacls "\\192.168.10.25\SDriver\Thanhan\Toilampbl4" /remove "PBL4\XPhuc" /T
+icacls "\\192.168.10.25\SDriver\Thanhan\Toilampbl4" /grant "PBL4\XPhuc":(OI)(CI)M
+icacls "\\192.168.10.25\SDriver\Thanhan\Toilampbl4\*" /grant "PBL4\XPhuc:(OI)(CI)M" /T
